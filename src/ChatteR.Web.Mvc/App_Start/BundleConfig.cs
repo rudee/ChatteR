d6467dc95ceb855ab54ffe210f9e55765470c8b0
Rudy Lee <rudyleehs@gmail.com>
2013-04-02 22:27:51 +0800

    Modified JavaScript references for CDN

--- src/ChatteR.Web.Mvc/App_Start/BundleConfig.cs
+++ src/ChatteR.Web.Mvc/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web;
+using System.Web.Configuration;
 using System.Web.Optimization;
 
 namespace ChatteR.Web.Mvc
@@ -7,8 +8,20 @@ namespace ChatteR.Web.Mvc
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            
+            bundles.UseCdn = true;
+
             bundles.Add(new StyleBundle("~/css/home/index")
                    .Include("~/css/home/index.css"));
+
+            bundles.Add(new ScriptBundle("~/js/jquery",
+                                         WebConfigurationManager.AppSettings["JqueryCdnUrl"])
+                   .Include("~/js/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/js/jquery.signalR",
+                                         WebConfigurationManager.AppSettings["JquerySignalRCdnUrl"])
+                   .Include("~/js/jquery.signalR-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/js/chatter")
                    .Include("~/js/console-fix.js", 
                             "~/js/chatter.js",
